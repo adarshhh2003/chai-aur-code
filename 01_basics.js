dbaf362d832edf90e972ef585f2ff3a0eb258e1e
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2024-03-14 00:20:45 +0530

    new file for learning datatypes of javascript

---
+++ 01_basics.js
@@ -0,0 +1,14 @@
+const accountId = 425235
+let accountEmail = "ap35@gmail.com"
+var accountPassword = "12344"
+accountCity = "bhopal"
+
+// accountId = 4    Not allowed 
+accountEmail = "apakf@gmail.com"
+accountPassword = "824758"
+accountCity = "indore"
+
+
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
