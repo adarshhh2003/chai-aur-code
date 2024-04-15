a8932cbaf7be2c9f4f628f56e0ea355c1b6a2d03
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2024-04-16 03:21:58 +0530

    learning scope level and mini hosting in javascript

--- scopes.js
+++ scopes.js
@@ -4,16 +4,55 @@ let a = 400
 
 if(true) {
     let a = 10
-    console.log("Inner: ",a)
+    // console.log("Inner: ",a)
 
     const b = 20
     var c = 30
 }
 
-for(let i=0; i<array.length; i++) {
-    const element = array[i];
-}
+// for(let i=0; i<array.length; i++) {
+//     const element = array[i];
+// }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+/*function one() {
+    const username = "Adarsh"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}   
+
+one()     */
+
+if(true) {
+    const username = "Adarsh"
+    if(username === "Adarsh") {
+        const lastname = " Patel"
+        console.log(username + lastname)
+    }
+    // console.log(lastname)
+}
+
+// console.log(username)
+
+// +++++++++++ Interesting +++++++++++
+
+console.log(addOne(5))
+
+function addOne(num) {
+    return num + 1
+}
+
+// This will give you errror because of holding the function
+// addTwo(7)
+
+const addTwo = function(num) {
+    return num + 2
+}
