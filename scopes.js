9f43aa66bc15030c0501840d539d50a1f7243200
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2024-04-16 03:01:24 +0530

    learning local and global scope in javascript

---
+++ scopes.js
@@ -0,0 +1,19 @@
+var c = 300
+
+let a = 400
+
+if(true) {
+    let a = 10
+    console.log("Inner: ",a)
+
+    const b = 20
+    var c = 30
+}
+
+for(let i=0; i<array.length; i++) {
+    const element = array[i];
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
