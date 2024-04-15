e4adaa421566dff84d8db8e9a6ab946f41abd9bd
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2024-04-16 00:38:21 +0530

    learning functions with objects in javascript

--- functions.js
+++ functions.js
@@ -34,4 +34,36 @@ function loginUserMessage(userName = "Sam") {
 }
 
 // console.log(loginUserMessage("adarsh"))
-console.log(loginUserMessage("Patel"))
+// console.log(loginUserMessage("Patel"))
+
+// ++++++++++
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    name: "Adarsh",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.name} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    name: "Sam",
+    price: 399
+})
+
+const myNewArray = [100, 300, 500, 700]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300,4500]))
