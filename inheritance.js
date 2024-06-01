fed3932c80bdbbd066c985f59ef846c9dc48998b
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2024-06-01 08:40:09 +0530

    learning inheritance in js

---
+++ inheritance.js
@@ -0,0 +1,30 @@
+class user {
+    constructor(username) {
+        this.username = username
+    }
+
+    logme() {
+        console.log(`Username is ${this.username}`);
+    }
+}
+
+class teacher extends user {
+    constructor(username, email, password) {
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addcourse() {
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new teacher("adarsh", "adarsh@email", "123abc")
+chai.addcourse();
+
+const masalachai = new user("masalachai")
+masalachai.logme();
+
+
+console.log(chai instanceof teacher);
